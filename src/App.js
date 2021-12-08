67ba82b3cdf35a85245546cb15a640857df0256f
Victor Emmanuel <victor_emanuel9@hotmail.com>
2021-12-08 16:04:21 -0300

    rota not found

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import RecipesProvider from './context/RecipesProvider';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Bebidas from './components/Bebidas';
 import Comidas from './components/Comidas';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <RecipesProvider>
     <Switch>
-      <Route path='/' component={} />
-      <Route path='/comidas' component={ Bebidas } />
-      <Route path='/bebidas' component={ Comidas } />
-      <Route path='/comidas/:id-da-receita' render={} />
-      <Route path='/bebidas/:id-da-receita' render={} />
-      <Route path='/comidas/:id-da-receita/in-progress' render={} />
-      <Route path='/bebidas/:id-da-receita/in-progress' render={} />
-      <Route path='/explorar' component={} />
-      <Route path='/explorar/comidas' component={} />
-      <Route path='/explorar/bebidas' component={} />
-      <Route path='/explorar/comidas/ingredientes' component={} />
-      <Route path='/explorar/bebidas/ingredientes' component={} />
-      <Route path='/explorar/comidas/area' component={} />
-      <Route path='/perfil' component={} />
-      <Route path='/receitas-feitas' component={} />
-      <Route path='/receitas-favoritas' component={} />
+      <Route exact path='/' component={} />
+      <Route exact path='/comidas' component={ Comidas } />
+      <Route exact path='/bebidas' component={ Bebidas } />
+      <Route exact path='/comidas/:id-da-receita' render={} />
+      <Route exact path='/bebidas/:id-da-receita' render={} />
+      <Route exact path='/comidas/:id-da-receita/in-progress' render={} />
+      <Route exact path='/bebidas/:id-da-receita/in-progress' render={} />
+      <Route exact path='/explorar' component={} />
+      <Route exact path='/explorar/comidas' component={} />
+      <Route exact path='/explorar/bebidas' component={} />
+      <Route exact path='/explorar/comidas/ingredientes' component={} />
+      <Route exact path='/explorar/bebidas/ingredientes' component={} />
+      <Route exact path='/explorar/comidas/area' component={} />
+      <Route exact path='/perfil' component={} />
+      <Route exact path='/receitas-feitas' component={} />
+      <Route exact path='/receitas-favoritas' component={} />
+      <Route path='*' component={ NotFound } />
     </Switch>
     </RecipesProvider>
   );
