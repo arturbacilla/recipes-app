330e7be45e7823285ee7ef9549ab59bc48fad786
Victor Emmanuel <victor_emanuel9@hotmail.com>
2021-12-09 19:29:17 -0300

    css header

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 function SearchBar() {
   return (
     <div className="searchBar-container">
+
       <div className="searchBar">
         <input type="text" name="" data-testid="search-input" />
+        <button
+          type="button"
+          className="searchBar-btn"
+          data-testid="exec-search-btn"
+        >
+          Buscar
+        </button>
       </div>
       <ul className="searchBar-radios">
         <li data-testid="ingredient-search-radio">
-          <label htmlFor="ingredient">
+          <label className="radio-selection" htmlFor="ingredient">
             Ingrediente
             <input type="radio" name="selection" id="ingredient" />
           </label>
@@ -26,9 +34,6 @@ function SearchBar() {
           </label>
         </li>
       </ul>
-      <div>
-        <button type="button" data-testid="exec-search-btn">buscar</button>
-      </div>
     </div>
   );
 }
