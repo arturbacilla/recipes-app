d88ccade7a00943de850eea03f64b8d88ba069da
Gabriel <gncorreia@protonmail.com>
2021-12-09 19:35:34 +0000

    profile - requirement 82 until 87 done

---
+++ src/components/ReceitasFavoritas/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function index() {
+  return (
+    <div>
+      <h2>Receitas Favoritas</h2>
+    </div>
+  );
+}
---
+++ src/components/ReceitasFeitas/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function index() {
+  return (
+    <div>
+      <h3>Receitas Feitas</h3>
+    </div>
+  );
+}
