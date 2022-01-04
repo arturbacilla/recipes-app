af263c9c1df193f00fb2b4c6c25811bb9cbbdcc3
Victor Emmanuel <victor_emanuel9@hotmail.com>
2022-01-04 17:14:53 -0300

    correcao footer - footer adicionado no explore: ingredient, byorigin, recipes; Profile.

--- src/components/Explore/ByIngredients.jsx
+++ src/components/Explore/ByIngredients.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header';
+import Footer from '../Footer';
 
 function ExploreByIngredients() {
   return (
     <>
       <Header name="Explorar Ingredientes" />
-      <section />
+      <section> </section>
+      <Footer />
     </>
   );
 }
