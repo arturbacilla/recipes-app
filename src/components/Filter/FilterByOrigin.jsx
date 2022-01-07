d24fdc447f8d2c382540387b84ad21121a5665c7
Gabriel <76435193+gncorreia@users.noreply.github.com>
2022-01-07 19:19:57 -0300

    lint fixed

--- src/components/Filter/FilterByOrigin.jsx
+++ src/components/Filter/FilterByOrigin.jsx
@@ -1,9 +1,15 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 function FilterByOrigin({ apiType }) {
+  console.log(apiType);
   return (
     <div />
   );
 }
+
+FilterByOrigin.propTypes = {
+  apiType: PropTypes.string.isRequired,
+};
 
 export default FilterByOrigin;
