0ea7b3bc421d2b9750ddf5c92150f3050ca047b9
Gabriel <76435193+gncorreia@users.noreply.github.com>
2022-01-07 19:11:28 -0300

    merged with the master

---
+++ src/components/Details/test.js
@@ -0,0 +1,32 @@
+/* eslint-disable quotes */
+const ingredient = [
+  'Lentils',
+  'Onion',
+  'Carrots',
+  'Tomato Puree',
+  'Cumin',
+  'Paprika',
+  'Mint',
+  'Thyme',
+  'Black Pepper',
+  'Red Pepper Flakes',
+  'Vegetable Stock',
+  'Water',
+  'Sea Salt',
+  '',
+  '',
+];
+
+function filterIngredients(array) {
+  const filtered = [];
+  array.forEach((curr) => {
+    if (curr !== null && curr.length > 0) {
+      filtered.push(curr);
+    }
+  });
+
+  return filtered;
+}
+
+console.log(filterIngredients(ingredient));
+console.log(ingredient[13] !== null);
